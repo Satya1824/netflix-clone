799ca68ba5e74f26c84f962daeebf2cf85593a60
sneha singh <snehaksingh18@gmail.com>
2024-02-06 06:23:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
