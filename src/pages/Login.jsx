a2ef0ffce468abd84e2b59f3a731f3c7be5328fd
sneha singh <snehaksingh18@gmail.com>
2024-02-07 23:21:39 +0530

    add login and signup pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import "../styles/auth.css";
-import { auth, logo } from "../assets";
+import { logo } from "../assets";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
       <div className="auth-cnt">
-        {/* background image */}
-        <div className="bg">
-          <img src={auth} alt="Background Image" />
-        </div>
-
-        {/* logo container */}
         <div className="logo-cnt">
           <img src={logo} alt="Netflix" />
         </div>
+        <div className="auth-box-cnt">
+          <div className="auth-wrap">
+            <h1>Sign In</h1>
+            <div className="inp-cnt">
+              <input type="email" placeholder="Email or phone number" />
+            </div>
+
+            <div className="inp-cnt">
+              <input type="password" placeholder="Password" />
+            </div>
+            <button>Sign In</button>
+            <div className="fp">
+              <a href="">Forgot password?</a>
+            </div>
+             
+             <div className="ntn">
+              <span>New to Netflix?</span>
+              <Link to="/signup">Sign up now.</Link>
+             </div>
+          </div>
+        </div>
       </div>
+
+      {/* background image */}
+      <div className="bg-img"></div>
     </div>
   );
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import "../styles/auth.css";
+import { logo } from "../assets";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
-};
+  return (
+    <div>
+      <div className="auth-cnt">
+        <div className="logo-cnt">
+          <img src={logo} alt="Netflix Logo" />
+        </div>
 
+        <div className="sgn-box-cnt">
+          <div className="sgn-cnt">
+            <h1>Sign Up</h1>
+
+            <div className="ip-cnt">
+              <input type="text" placeholder="Username" />
+            </div>
+
+            <div className="ip-cnt">
+              <input type="email" placeholder="Your email address" />
+            </div>
+
+            <div className="ip-cnt">
+              <input type="password" placeholder="Create a password" />
+            </div>
+
+            <div className="ip-cnt">
+              <input type="password" placeholder="Confirm password" />
+            </div>
+            <button type="submit">Sign Up</button>
+
+            <div className="au">
+            <span>Already a user?</span>
+            <Link to="/login">LOGIN</Link>
+           </div>  
+          </div>
+
+           
+        </div>
+      </div>
+
+      {/* background image */}
+      <div className="bg-img"></div>
+    </div>
+  );
+};
 export default Signup;
